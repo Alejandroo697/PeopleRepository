b6bcca840c845389461226056b5bf2d16f1d4ceb
CDS2UNIVO13 <CDS2UNIVO13@CDS2UNIVO13>
2019-09-05 09:09:14 -0600

    PersonRepositoryadded

---
+++ Pleope/Pleope/Pleope/PeronRepositoryCRUD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using Pleope.Models;
+
+namespace Pleope
+{
+    class PeronRepositoryCRUD
+    {
+        SQLiteConnection conn;
+        public string StatusMessage { get; set; }
+
+        public PeronRepositoryCRUD(string dbPath)
+        {
+            // Creamos la conexion.
+            conn = new SQLiteConnection(dbPath);
+            conn.CreateTable<Person>();
+            
+
+        }
+    }
+}
