ab08dc5c6c80ddd5a0a66ef75a8ea97baae8fbfc
CDS2UNIVO13 <CDS2UNIVO13@CDS2UNIVO13>
2019-09-05 08:41:33 -0600

    ClassModelAgregar

---
+++ Pleope/Pleope/Pleope/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+namespace Pleope.Models
+{
+    [Table("T_People")]
+  public  class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
